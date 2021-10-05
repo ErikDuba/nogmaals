c585e62f34dbfe6398731702af95a8413ce64b59
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-10-05 09:26:25 +0200

    no while tho

--- submap_duizelig/dagen.py
+++ submap_duizelig/dagen.py
@@ -1,5 +1,21 @@
 import time
 import os
-
-whichDay = input(str('Welke dag van de week?: '))
-
+def clear():
+    command = 'cls'
+    os.system(command)
+clear()
+whichDay = input(str('Welke dag van de week? ma, di, wo, do, vr, za, zo: '))
+if whichDay == 'ma':
+    print('dinsdag, woensdag, donderdag, vrijdag, zaterdag, zondag, maandag')
+elif whichDay == 'di':
+    print('woensdag, donderdag, vrijdag, zaterdag, zondag, maandag, dinsdag')
+elif whichDay == 'wo':
+    print('donderdag, vrijdag, zaterdag, zondag, maandag, dinsdag, woensdag')
+elif whichDay == 'do':
+    print('vrijdag, zaterdag, zondag, maandag, dinsdag, woensdag, donderdag')
+elif whichDay == 'vr':
+    print('zaterdag, zondag, maandag, dinsdag, woensdag, donderdag, vrijdag')
+elif whichDay == 'za':
+    print('zondag, maandag, dinsdag, woensdag, donderdag, vrijdag, zaterdag')
+elif whichDay == 'zo':
+    print('maandag, dinsdag, woensdag, donderdag, vrijdag, zaterdag, zondag')
