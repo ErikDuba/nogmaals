d80db1b0c7fa6c70473b534243877965e66f2390
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-10-05 14:20:26 +0200

    .

---
+++ submap_duizelig/50tot1000.py
@@ -0,0 +1,4 @@
+getal = 49
+while getal < 1000:
+    getal = getal + 1
+    print(getal) 
--- submap_duizelig/quit.py
+++ submap_duizelig/quit.py
@@ -4,11 +4,13 @@ def clear():
     command = 'cls'
     os.system(command)
 clear()
+getal = 0 
 saus = ''
 while saus != 'quit':
     saus = str(input('Guess the word, it starts with a \'q\' and ends with \'uit\': '))
     if saus == 'quit':
-        print('Good geradeerd')
+        getal = getal + 1 
+        print(getal + 'Good geradeerd')
         time.sleep(1)                  
     else:
         print('Try again: ')   
