465f71330f4fc57d90ca7b7a915eb3253d2ec1e3
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-10-06 11:58:30 +0200

    laatste opdracht

--- submap_duizelig/50tot1000.py
+++ submap_duizelig/50tot1000.py
@@ -1,4 +1,7 @@
-getal = 49
-while getal < 1000:
-    getal = getal + 1
-    print(getal) 
+getal = 50
+getal1 = 51
+while getal < 1100:    
+    print(getal)
+    getal = getal1 + getal
+    getal1 = getal1 + 1 
+    
